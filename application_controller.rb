bc67d466f2d44006d0adaee853bcd1c52aac8597
Dammy Akinfenwa <dammy.akinfenwa@gmail.com>
2015-07-01 17:35:22 +0000

    my first commit

---
+++ application_controller.rb
@@ -0,0 +1,22 @@
+require 'bundler'
+Bundler.require
+
+class ApplicationController < Sinatra::Base
+
+  get '/' do
+    erb :index
+  end
+  
+  get '/preview' do
+    erb :index # Brian's test
+  end
+
+	get '/preview' do
+    erb :index # Brian's test
+  end
+	
+	post '/generate_message' do
+		erb :generated # Dammy's test
+	end
+
+end
